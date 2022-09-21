48763d916cd1cbf8b474441b2dd50bb5093a215f
pablo-lucas <pablo.lucv@gmail.com>
2022-09-21 09:35:28 -0500

    Added Producer with callback

--- kafka-basics/src/main/java/io/demo/ProducerDemoWithCallback.java
+++ kafka-basics/src/main/java/io/demo/ProducerDemoWithCallback.java
@@ -1,19 +1,17 @@
 package io.demo;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
 
-public class ProducerDemo {
-
+public class ProducerDemoWithCallback {
     private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+
     public static void main(String[] args) {
-        log.info("Kafka Producer");
+        log.info("Kafka Producer With Callback");
 
         Properties properties = new Properties();
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
@@ -28,7 +26,21 @@ public class ProducerDemo {
                 new ProducerRecord<>("demo", "Hello world!");
 
         //send the data - asynchronous
-        producer.send(producerRecord);
+        producer.send(producerRecord, new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata metadata, Exception e) {
+                // executes every time a record is successfully sent or an exception is thrown
+                if (e == null){
+                    log.info("Received new metadata / \n" +
+                            "Topic: " + metadata.topic() + "\n" +
+                            "Partition: " + metadata.partition()+ "\n" +
+                            "Offset: " + metadata.offset() + "\n" +
+                            "Timestamp: " + metadata.timestamp());
+                } else {
+                    log.error("Error while producing", e);
+                }
+            }
+        });
 
         // flush data - synchronous
         producer.flush();
